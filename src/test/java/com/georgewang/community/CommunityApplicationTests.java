3358bd6e50e54ee34e31e76086e733c6a968cc42
UncleGeorge <george.cosmoswill@gmail.com>
2020-05-15 22:08:59 -0400

    init repo

---
+++ src/test/java/com/georgewang/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgewang.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
